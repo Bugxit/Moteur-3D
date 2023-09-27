e6bf3ff5e349b854ea3263eef4533ee16e466cfc
Bugxit <101186137+Bugxit@users.noreply.github.com>
2023-09-27 11:42:14 +0200

    Add files via upload

--- Moteur3Dquimarche.py
+++ Moteur3Dquimarche.py
@@ -2,7 +2,20 @@ import turtle as tu
 import math as m
 import time as ti
 
+tu.hideturtle()
+
 tu.Screen().tracer(0)
+class Point():
+    
+    def __init__(self, point_coords):
+        self.point_coords = point_coords
+        
+    def turn(self, center_coords):
+        tu.up()
+        tu.goto(center_coords[0], center_coords[1])
+        tu.seth(tu.towards(self.point_coords[0], self.point_coords[1])+1)
+        tu.forward(m.sqrt((center_coords[0]-self.point_coords[0])**2+(center_coords[1]-self.point_coords[1])**2))
+        self.point_coords = [tu.xcor(), tu.ycor()]
 
 class Spec(tu.Turtle):
 
@@ -23,25 +36,153 @@ class Spec(tu.Turtle):
     screen_center_coords = [self.xcor(), self.ycor()]
     self.goto(temporary_spec_coords[0], temporary_spec_coords[1])
     self.seth(0)
-    while round(self.towards(screen_center_coords[0], screen_center_coords[1])) != self.rotation+90 and round(self.towards(screen_center_coords[0], screen_center_coords[1])) != self.rotation-90 and self.ycor() <= 540:
+    cancel = 0
+    save = round(self.towards(screen_center_coords[0], screen_center_coords[1]))
+    while cancel != 1 and round(self.towards(screen_center_coords[0], screen_center_coords[1])) != self.rotation+90 and round(self.towards(screen_center_coords[0], screen_center_coords[1])) != self.rotation-90 and self.ycor() <= 540:
+        if round(self.towards(screen_center_coords[0], screen_center_coords[1])) >= self.rotation+90 >= save:
+            cancel = 1
+            self.seth(self.towards(temporary_point_coords[0], temporary_point_coords[1]))
+            self.backward(2)
+        elif round(self.towards(screen_center_coords[0], screen_center_coords[1])) <= self.rotation-90 <= save:
+            cancel = 1
+            self.seth(self.towards(temporary_point_coords[0], temporary_point_coords[1]))
+            self.backward(2)
         self.seth(self.towards(temporary_point_coords[0], temporary_point_coords[1]))
+        save = round(self.towards(screen_center_coords[0], screen_center_coords[1]))
         self.forward(1)
         self.seth(abs(m.floor(self.towards(screen_center_coords[0], screen_center_coords[1]))))
-    self.new_point_coords.append(round(self.xcor()))
-    tu.Screen().update()
+    self.new_point_coords = (round(self.xcor()))
+    
+p1 = Point([-50, 150])
+p2 = Point([50, 150])
+p3 = Point([50, 250])
+p4 = Point([-50,250])
 
+p5 = Point([-50, 150])
+p6 = Point([50, 150])
+p7 = Point([50, 250])
+p8 = Point([-50,250])
+def generate_cube1():
+    for i in range(90):
+        tu.clear()
+        tu.up()
+        s1 = Spec((0, 0, 0), (100,100, 100), 90, 100)
+        p1.turn((0, 200))
+        p2.turn((0, 200))
+        p3.turn((0, 200))
+        p4.turn((0, 200))
+        p5.turn((0, 200))
+        p6.turn((0, 200))
+        p7.turn((0, 200))
+        p8.turn((0, 200))
+        
+        tu.up()
+        s1.check_screen((0,0), (p1.point_coords[0], p1.point_coords[1]))
+        tu.goto(s1.new_point_coords, 0)
+        s1.check_screen((0,0), (50, p1.point_coords[1]))
+        tu.goto(tu.xcor(), s1.new_point_coords)
+        p1_save=(tu.xcor(), tu.ycor())
+        tu.down()
+        tu.dot(10, 'black')
+        
+        tu.up()
+        s1.check_screen((0,0), (p2.point_coords[0], p2.point_coords[1]))
+        tu.goto(s1.new_point_coords, 0)
+        s1.check_screen((0,0), (50, p2.point_coords[1]))
+        tu.goto(tu.xcor(), s1.new_point_coords)
+        p2_save=(tu.xcor(), tu.ycor())
+        tu.down()
+        tu.dot(10, 'red')
+        
+        tu.up()
+        s1.check_screen((0,0), (p3.point_coords[0], p3.point_coords[1]))
+        tu.goto(s1.new_point_coords, 0)
+        s1.check_screen((0,0), (50, p3.point_coords[1]))
+        tu.goto(tu.xcor(), s1.new_point_coords)
+        p3_save=(tu.xcor(), tu.ycor())
+        tu.down()
+        tu.dot(10, 'blue')
+        
+        tu.up()
+        s1.check_screen((0,0), (p4.point_coords[0], p4.point_coords[1]))
+        tu.goto(s1.new_point_coords, 0)
+        s1.check_screen((0,0), (50, p4.point_coords[1]))
+        tu.goto(tu.xcor(), s1.new_point_coords)
+        p4_save=(tu.xcor(), tu.ycor())
+        tu.down()
+        tu.dot(10, 'green')
+        
+        tu.up()
+        s1.check_screen((0,0), (p5.point_coords[0], p5.point_coords[1]))
+        tu.goto(s1.new_point_coords, 0)
+        s1.check_screen((0,0), (-50, p5.point_coords[1]))
+        tu.goto(tu.xcor(), s1.new_point_coords)
+        p5_save=(tu.xcor(), tu.ycor())
 
-S1 = Spec((0, 0, 0), (100,100, 100), 90, 100)
-S1.check_screen((0,0), (100, 100))
-S1.check_screen((0,0), (100, 100))
-print(S1.new_point_coords)
-tu.goto(S1.new_point_coords[0], S1.new_point_coords[1])
-tu.dot(10)
+        tu.down()
+        tu.dot(10, 'black')
+        tu.up()
+        s1.check_screen((0,0), (p6.point_coords[0], p6.point_coords[1]))
+        tu.goto(s1.new_point_coords, 0)
+        s1.check_screen((0,0), (-50, p6.point_coords[1]))
+        tu.goto(tu.xcor(), s1.new_point_coords)
+        p6_save=(tu.xcor(), tu.ycor())
 
-S2 = Spec((0, 0, 0), (100,100, 200), 90, 100)
-S2.check_screen((0,0), (100, 200))
-S2.check_screen((0,0), (100, 200))
-print(S2.new_point_coords)
-tu.goto(S2.new_point_coords[0], S2.new_point_coords[1])
-tu.dot(10)
-tu.done()
+        tu.down()
+        tu.dot(10, 'red')
+        tu.up()
+        s1.check_screen((0,0), (p7.point_coords[0], p7.point_coords[1]))
+        tu.goto(s1.new_point_coords, 0)
+        s1.check_screen((0,0), (-50, p7.point_coords[1]))
+        tu.goto(tu.xcor(), s1.new_point_coords)
+        p7_save=(tu.xcor(), tu.ycor())
+
+        tu.down()
+        tu.dot(10, 'blue')
+        tu.up()
+        s1.check_screen((0,0), (p8.point_coords[0], p8.point_coords[1]))
+        tu.goto(s1.new_point_coords, 0)
+        s1.check_screen((0,0), (-50, p8.point_coords[1]))
+        tu.goto(tu.xcor(), s1.new_point_coords)
+        p8_save=(tu.xcor(), tu.ycor())
+        tu.down()
+        tu.dot(10, 'green')
+        
+        tu.up()
+        tu.goto(p1_save[0], p1_save[1])
+        tu.down()
+        tu.goto(p2_save[0], p2_save[1])
+        tu.goto(p3_save[0], p3_save[1])
+        tu.goto(p4_save[0], p4_save[1])
+        tu.goto(p1_save[0], p1_save[1])            
+        tu.up()
+        tu.goto(p5_save[0], p5_save[1])
+        tu.down()
+        tu.goto(p6_save[0], p6_save[1])
+        tu.goto(p7_save[0], p7_save[1])
+        tu.goto(p8_save[0], p8_save[1])
+        tu.goto(p5_save[0], p5_save[1])
+        
+        tu.up()
+        tu.goto(p5_save[0], p5_save[1])
+        tu.down()
+        tu.goto(p1_save[0], p1_save[1])
+        tu.up()
+        tu.goto(p6_save[0], p6_save[1])
+        tu.down()
+        tu.goto(p2_save[0], p2_save[1])
+        tu.up()
+        tu.goto(p7_save[0], p7_save[1])
+        tu.down()
+        tu.goto(p3_save[0], p3_save[1])
+        tu.up()
+        tu.goto(p8_save[0], p8_save[1])
+        tu.down()
+        tu.goto(p4_save[0], p4_save[1])
+        
+        tu.Screen().update()
+
+while True:
+    generate_cube1()
+
+tu.Screen().update()
